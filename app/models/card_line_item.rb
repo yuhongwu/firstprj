744c16759afda2a9b1edf6324fd21c2fb233b5b6
yuhongwu <wyh770406@gmail.com>
2013-07-16 15:51:17 +0800

    first commit

---
+++ app/models/card_line_item.rb
@@ -0,0 +1,20 @@
+class CardLineItem < ActiveRecord::Base
+  belongs_to :card
+  belongs_to :order
+
+  scope :without_order, lambda { where('order_id is null') }
+
+  validates :card, :amount, :validity, presence: true
+  validates :validity, numericality: { greater_than_or_equal_to: 0 }
+
+  default_value_for :validity, 0
+  
+  after_create :update_card!
+
+  protected
+
+  def update_card!
+    card.update!
+    card.increase_validity!(self.validity)
+  end
+end
